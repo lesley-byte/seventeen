d8520ca75b0b4627d75f753fd0765740c7cb9273
lesley-byte <60296103+lesley-byte@users.noreply.github.com>
2021-03-22 15:29:11 -0400

    Add files via upload

--- yearseventeen.py
+++ yearseventeen.py
@@ -1,19 +1,19 @@
 import PySimpleGUI as sg
 
 sg.theme('DarkAmber')
-layout = [ [sg.Text("when did you turn 17?")],
-           [sg.Text("Year of birth:"), sg.InputText()],
-           [sg.Button('Ok'), sg.Button('Cancel')]]
+layout = [ [sg.Text("When did you turn 17? \n(don't use backspace or delete, just highlight and type over if new year needed)\n")],
+           [sg.Text("Year of birth:"), sg.InputText(enable_events=True)],
+           [sg.Text("You turned 17 in:\n")],
+           [sg.Text(size=(15,1), key='-OUTPUT-')]]
 
-window = sg.Window('Year 17', layout)
+window = sg.Window('Lesley Year 17 Calculator', layout)
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED or event == 'Cancel':
+    if event == sg.WIN_CLOSED:
         break
     s = values[0]
     s1 = int(s)
-    s2  = s1 + 17
-    sg.popup('you were 17 in', s2)
-    
-    
+    s2  = s1 + 17   
+    if values[0]:
+        window['-OUTPUT-'].update(s2)
 window.close()
