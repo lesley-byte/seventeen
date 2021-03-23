341bf78f93cf1efc2473e2f749ed7c637aacd023
lesley-byte <60296103+lesley-byte@users.noreply.github.com>
2021-03-22 20:06:41 -0400

    Add files via upload

--- yearseventeen.py
+++ yearseventeen.py
@@ -1,19 +1,32 @@
 import PySimpleGUI as sg
 
 sg.theme('DarkAmber')
-layout = [ [sg.Text("When did you turn 17? \n(don't use backspace or delete, just highlight and type over if new year needed)\n")],
-           [sg.Text("Year of birth:"), sg.InputText(enable_events=True)],
+layout = [ [sg.Text("When did you turn 17? ")],
+           [sg.Text("Year of birth:"), sg.Input(enable_events=True)],
            [sg.Text("You turned 17 in:\n")],
-           [sg.Text(size=(15,1), key='-OUTPUT-')]]
+           [sg.Text(size=(30,1), key='-OUTPUT-')]]
 
 window = sg.Window('Lesley Year 17 Calculator', layout)
 while True:
     event, values = window.read()
-    if event == sg.WIN_CLOSED:
+    if event == sg.WIN_CLOSED or event is None:
         break
-    s = values[0]
-    s1 = int(s)
-    s2  = s1 + 17   
-    if values[0]:
+    try:
+        s = values[0]
+        s0 = float(s)
+        s1 = int(s0)
+        s2  = s1 + 17
+        s3 = "*Enter a valid year of birth*"
+    except ValueError:
+        s = 0
+        
+        continue
+    if values[0] and (s2 > 1000):
         window['-OUTPUT-'].update(s2)
+        
+    else:
+        window['-OUTPUT-'].update(s3)
+        continue
 window.close()
+#except ValueError:
+#    print("invalid") 
